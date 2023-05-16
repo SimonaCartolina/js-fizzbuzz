5b670b5a5e01d8dc0d7531f255ff2d7b12d06075
Simona Cartolina <simonacartolina@hotmail.com>
2023-05-16 16:41:33 +0200

    modified i=0 with i=1

--- myscript.js
+++ myscript.js
@@ -1,7 +1,7 @@
 
 const ulElement= document.querySelector('ul' );
 for(
-    let i=0; i<=100; i= i + 1) {
+    let i=1; i<=100; i= i + 1) {
         const li= document.createElement('li');
         li.append('elemento: ' + i);
         ulElement.appendChild(li);
