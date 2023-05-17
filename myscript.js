dd579cd5c00ab1b476ae74a0b83225054e1dae4f
Simona Cartolina <simonacartolina@hotmail.com>
2023-05-17 12:43:44 +0200

    corrected exercise and made the front part

--- myscript.js
+++ myscript.js
@@ -4,17 +4,18 @@ for(
     let i=1; i<=100; i= i + 1) {
         const li= document.createElement('li');
 
-        if(i % 3===0) {
+        if(i % 3===0 && i % 5===0) {
+            console.log('FizzBuzz');
+            li.style.color="yellow";
+        }
+
+        else if(i % 3===0) {
             console.log('Fizz');
             li.style.color="red";
         }
         else if(i % 5===0) {
             console.log ('Buzz');
             li.style.color="green";
-        }
-        else if(i % 3===0 && i % 5===0) {
-            console.log('FizzBuzz');
-            li.style.color="yellow";
         }
         else if(i % 3!=0 && i % 5!=0){
             li.style.color="brown";
